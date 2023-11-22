99bc30478ade9f193b27f902e249c39425f492ea
Charles Cook <charlescook80@outlook.com>
2023-11-22 15:36:27 +0000

    add style to navbar

--- src/features/NavBar/index.jsx
+++ src/features/NavBar/index.jsx
@@ -7,19 +7,19 @@ import logo from '../../media/cookie-logo.svg'
 
 function NavBar() {
     return (
-      <div className="navbar">
-        <div className="navbar-logo">
+      <div className=" text-white relative flex flex-row h-16 bg-zinc-800 space-x-1 items-center justify-center">
+        <div className="absolute left-0 text-2xl hidden md:block ">
           {/*<img src={logo} alt="logo" />*/}
           <span className='title'>Cookie-It</span>
         </div>
-        <form className="search" >
-          <input
+        <form class="space-x-3 text-black">
+          <input class="rounded-lg md:w-96 p-2"
             type="text"
             placeholder="Search"
             aria-label="Search posts"
           />
           <button type="submit" aria-label="Search">
-            <FontAwesomeIcon icon={faMagnifyingGlass} style={{color: "#000000",}} />
+            <FontAwesomeIcon icon={faMagnifyingGlass} style={{color: "#FFFFFF",}} />
           </button>
         </form>
       </div>
